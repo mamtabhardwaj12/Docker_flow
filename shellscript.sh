8c360c0c040ef148ec131cc7abb0d4e06b3188f1
Mamta <mamtabharadwaj12@gmail.com>
2019-07-17 05:36:09 +0000

    third push

---
+++ shellscript.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "execute command"
+
+echo "$su mqbrkrs"
+
+echo "$mqbrkrs"
+
+echo "$mqsistart BLBRKT01"
+
+mqsideploy BLBRKT01 -e DEMO -a /home/united/iib/docker.bar
+echo "en"
+
