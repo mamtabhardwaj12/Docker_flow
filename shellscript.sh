c287c3e91a14f97cc881cfff203bc5c0b38e417f
Dell <priya.yadav.central@gmail.com>
2019-07-19 12:23:20 +0530

    shellscript update

--- shellscript.sh
+++ shellscript.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 echo "execute command"
 
-echo "$su mqbrkrs"
-
-echo "$mqbrkrs"
-
 echo "$mqsistart BLBRKT01"
 
-mqsideploy BLBRKT01 -e DEMO -a /home/united/iib/docker.bar
+mqsideploy BLBRKT01 -e DEMO -a /home/united/iib/iib_bars/Docker_flow/docker.bar
 echo "en"
 
